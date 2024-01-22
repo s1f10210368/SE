e46896f7d07aa2ed30e4d548e09e9e7d13e11088
s1f10210368 <s1f102103684@iniad.org>
2024-01-22 16:52:12 +0900

    Lower case

---
+++ 2022final/DisplayLower.java
@@ -0,0 +1,22 @@
+/*
+ * 以下の要件を満たすpublicなクラス DisplayLower を宣言しなさい。
+
+    実行すると、標準入力からの入力を受付け、入力された文字列を小文字にして表示する。
+    空文字列がされると、終了する。
+ */
+
+import java.util.Scanner;
+
+public class DisplayLower {
+   public static void main(String args[]){
+      Scanner scanner = new Scanner(System.in);
+      for (;;){
+         String str = scanner.nextLine();
+         if(str.length() == 0){
+            break;
+         }
+         System.out.println(str.toLowerCase());
+      }
+      scanner.close();
+   }
+}
